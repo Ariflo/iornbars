ffd4050fe82aa5dea6f966a58cb76ebf4eb4f079
AriFlo <ariflo@arianflores.com>
2016-03-19 12:22:49 -0700

    setup jumbotron

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,14 @@
 iornBars.controller('mainController', ['$scope', '$http', '$parse', '$location', '$routeParams', '$timeout', '$anchorScroll', 'anchorSmoothScroll',
 	                                     function($scope,  $http,  $parse,  $location,   $routeParams, $timeout, $anchorScroll, anchorSmoothScroll) {
+	//Render bio button 
+	$timeout(function(){
+		$scope.show = true; 
+		$scope.changeImage = true; 
+	}, 2000); 	
 
-
+	$timeout(function(){
+		$scope.changeImage = false; 
+		$scope.changeImage2 = true; 
+	}, 4000); 
 
 }]);
