c7d98e21378e645b6520b5a441dcb32bff918fa0
AriFlo <ariflo@arianflores.com>
2016-03-19 14:02:27 -0700

    completed jumbotron images

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,18 +3,19 @@ iornBars.controller('mainController', ['$scope', '$http', '$parse', '$location',
 	//Render bio button 
 	$timeout(function(){
 		$scope.show = true; 
-	}, 2000); 	
+	}, 1000); 	
 
+	//Run background-image slide-show
 	$interval(function(){
 		$timeout(function(){
 				$scope.set_bgrd_image = function(){
 						return {  "background": "center url('/images/iornbars2.jpg')",
 							  "background-size": "cover",
 							   "background-repeat": "no-repeat",
-							   "-webkit-transition": "all linear 2s",
-							   "-moz-transition": "all linear 2s",
-							   "-o-transition": "all linear 2s",
-							   "transition": "all linear 2s"}			
+							   "-webkit-transition": "all linear 3s",
+							   "-moz-transition": "all linear 3s",
+							   "-o-transition": "all linear 3s",
+							   "transition": "all linear 3s"}			
 				}
 			}, 2000)		
 
@@ -23,12 +24,48 @@ iornBars.controller('mainController', ['$scope', '$http', '$parse', '$location',
 						return {  "background": "center url('/images/iornbars3.jpg')",
 							  "background-size": "cover",
 							   "background-repeat": "no-repeat",
-							   "-webkit-transition": "all linear 2s",
-							   "-moz-transition": "all linear 2s",
-							   "-o-transition": "all linear 2s",
-							   "transition": "all linear 2s"}			
+							   "-webkit-transition": "all linear 3s",
+							   "-moz-transition": "all linear 3s",
+							   "-o-transition": "all linear 3s",
+							   "transition": "all linear 3s"}			
 				}
-			}, 3000)
-	}, 3000); 
+			}, 6000)		
+
+		$timeout(function(){
+				$scope.set_bgrd_image = function(){
+						return {  "background": "center url('/images/iornbars4.jpg')",
+							  "background-size": "cover",
+							   "background-repeat": "no-repeat",
+							   "-webkit-transition": "all linear 3s",
+							   "-moz-transition": "all linear 3s",
+							   "-o-transition": "all linear 3s",
+							   "transition": "all linear 3s"}			
+				}
+			}, 10000)		
+
+		$timeout(function(){
+				$scope.set_bgrd_image = function(){
+						return {  "background": "center url('/images/iornbars5.jpeg')",
+							  "background-size": "cover",
+							   "background-repeat": "no-repeat",
+							   "-webkit-transition": "all linear 3s",
+							   "-moz-transition": "all linear 3s",
+							   "-o-transition": "all linear 3s",
+							   "transition": "all linear 3s"}			
+				}
+			}, 14000)		
+
+		$timeout(function(){
+				$scope.set_bgrd_image = function(){
+						return {  "background": "center url('/images/iornbars.jpg')",
+							  "background-size": "cover",
+							   "background-repeat": "no-repeat",
+							   "-webkit-transition": "all linear 3s",
+							   "-moz-transition": "all linear 3s",
+							   "-o-transition": "all linear 3s",
+							   "transition": "all linear 3s"}			
+				}
+			}, 18000)
+	}, 25000); 
 
 }]);
