df3c91711cbe3f7d135eb2ff4e03f7500d39aaef
AriFlo <ariflo@arianflores.com>
2016-03-19 13:16:33 -0700

    working on jumbotron image slides

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,14 +1,34 @@
-iornBars.controller('mainController', ['$scope', '$http', '$parse', '$location', '$routeParams', '$timeout', '$anchorScroll', 'anchorSmoothScroll',
-	                                     function($scope,  $http,  $parse,  $location,   $routeParams, $timeout, $anchorScroll, anchorSmoothScroll) {
+iornBars.controller('mainController', ['$scope', '$http', '$parse', '$location', '$routeParams', '$timeout', '$anchorScroll', '$interval', 'anchorSmoothScroll',
+	                                     function($scope,  $http,  $parse,  $location,   $routeParams, $timeout, $anchorScroll, $interval, anchorSmoothScroll) {
 	//Render bio button 
 	$timeout(function(){
 		$scope.show = true; 
-		$scope.changeImage = true; 
 	}, 2000); 	
 
-	$timeout(function(){
-		$scope.changeImage = false; 
-		$scope.changeImage2 = true; 
-	}, 4000); 
+	$interval(function(){
+		$timeout(function(){
+				$scope.set_bgrd_image = function(){
+						return {  "background": "center url('/images/iornbars2.jpg')",
+							  "background-size": "cover",
+							   "background-repeat": "no-repeat",
+							   "-webkit-transition": "all linear 2s",
+							   "-moz-transition": "all linear 2s",
+							   "-o-transition": "all linear 2s",
+							   "transition": "all linear 2s"}			
+				}
+			}, 2000)		
+
+		$timeout(function(){
+				$scope.set_bgrd_image = function(){
+						return {  "background": "center url('/images/iornbars3.jpg')",
+							  "background-size": "cover",
+							   "background-repeat": "no-repeat",
+							   "-webkit-transition": "all linear 2s",
+							   "-moz-transition": "all linear 2s",
+							   "-o-transition": "all linear 2s",
+							   "transition": "all linear 2s"}			
+				}
+			}, 3000)
+	}, 3000); 
 
 }]);
