e56092456c9d97619a25536aa673676f56e8a606
AriFlo <ariflo@arianflores.com>
2016-03-22 18:56:12 -0700

    added more population data

--- server/models/states.js
+++ server/models/states.js
@@ -1,5 +1,6 @@
 var  knex = require('../../db/knex');
 var statesPopData = require('../../seeds/boj_stats/StatePopulationData_2014.json');
+var popGenderData = require('../../seeds/boj_stats/prisonPopulationByStateandGender.json');
 
 // get all states
 var States = function(){
@@ -17,10 +18,11 @@ var updateState = function(state, stateInfo){
     state_name: state
   }).first().update(stateInfo).then(function(updatedState){});
 }
+
 // delete state
 var deleteState = function(state){
   return States().where({
-    name: state.state_name
+    state_name: state
   }).first().del();
 }
 
@@ -36,21 +38,27 @@ var  deleteAllStates = function(){
     knex('states').del();
 }
 
-// populated comapanies table with node server/models/companies
+// populated states table with node server/models/populateStates
 var populateDb = function(){
+  for(var i = 0; i < statesPopData.length; i += 3){
 
-//Entire state seed data 
-for(var i = 0; i < statesPopData.length; i += 3){
-      addState({
-        state_name: statesPopData[i + 2].Geography,
-        male_population: statesPopData[i+1].Total,
-        female_population: statesPopData[i].Total,
-        white_population: statesPopData[i + 2]['Race Alone - White'],
-        black_population: statesPopData[i + 2]['Race Alone - Black or African American'],
-        hispanic_population: statesPopData[i + 2]['Two or More Races'],
-        other_population: statesPopData[i + 2]['Race Alone - Asian'],
-      });
-}
+        var other = statesPopData[i + 2]['Race Alone - Asian'] + statesPopData[i + 2]['Race Alone - Native Hawaiian and Other Pacific Islander'];
+
+        addState({
+          state_name: statesPopData[i + 2].Geography,
+          male_population: statesPopData[i+1].Total,
+          female_population: statesPopData[i].Total,
+          white_population: statesPopData[i + 2]['Race Alone - White'],
+          black_population: statesPopData[i + 2]['Race Alone - Black or African American'],
+          hispanic_population: statesPopData[i + 2]['Two or More Races'],
+          other_population: other
+        });
+  }
+
+  for(var i = 0; i < popGenderData.length; i++){
+      updateState(popGenderData[i].Jurisdiction, {male_jailed_population: popGenderData[i].Male});
+      updateState(popGenderData[i].Jurisdiction, {female_jailed_population: popGenderData[i].Female});
+  }
 
 }
 
