ae48c28c666dc3c51d2792ea9edae15efab874b7
AriFlo <ariflo@arianflores.com>
2016-03-25 17:07:36 -0700

    set up for rvc

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -21,69 +21,73 @@ iornBars.controller('mainController', ['$rootScope', '$scope', '$http', '$parse'
 		$scope.displayAbout = !$scope.displayAbout;
 	}	
 
-	//Run background-image slide-show
-	$interval(function(){
-		$timeout(function(){
-				$scope.set_bgrd_image = function(){
-						return {  "background": "center url('/images/iornbars2.jpg')",
-							  "background-size": "cover",
-							   "background-repeat": "no-repeat",
-							   "-webkit-transition": "all linear 3s",
-							   "-moz-transition": "all linear 3s",
-							   "-o-transition": "all linear 3s",
-							   "transition": "all linear 3s"}			
-				}
-			}, 2000)		
+	// images have loaded
+	angular.element('.jumbotron').imagesLoaded( function() {
+	  //Run background-image slide-show
+		  $interval(function(){
+		  	$timeout(function(){
+		  			$scope.set_bgrd_image = function(){
+		  					return {  "background": "center url('/images/iornbars2.jpg')",
+		  						  "background-size": "cover",
+		  						   "background-repeat": "no-repeat",
+		  						   "-webkit-transition": "all linear 3s",
+		  						   "-moz-transition": "all linear 3s",
+		  						   "-o-transition": "all linear 3s",
+		  						   "transition": "all linear 3s"}			
+		  			}
+		  		}, 2000)		
 
-		$timeout(function(){
-				$scope.set_bgrd_image = function(){
-						return {  "background": "center url('/images/iornbars3.jpg')",
-							  "background-size": "cover",
-							   "background-repeat": "no-repeat",
-							   "-webkit-transition": "all linear 3s",
-							   "-moz-transition": "all linear 3s",
-							   "-o-transition": "all linear 3s",
-							   "transition": "all linear 3s"}			
-				}
-			}, 6000)		
+		  	$timeout(function(){
+		  			$scope.set_bgrd_image = function(){
+		  					return {  "background": "center url('/images/iornbars3.jpg')",
+		  						  "background-size": "cover",
+		  						   "background-repeat": "no-repeat",
+		  						   "-webkit-transition": "all linear 3s",
+		  						   "-moz-transition": "all linear 3s",
+		  						   "-o-transition": "all linear 3s",
+		  						   "transition": "all linear 3s"}			
+		  			}
+		  		}, 6000)		
 
-		$timeout(function(){
-				$scope.set_bgrd_image = function(){
-						return {  "background": "center url('/images/iornbars4.jpg')",
-							  "background-size": "cover",
-							   "background-repeat": "no-repeat",
-							   "-webkit-transition": "all linear 3s",
-							   "-moz-transition": "all linear 3s",
-							   "-o-transition": "all linear 3s",
-							   "transition": "all linear 3s"}			
-				}
-			}, 10000)		
+		  	$timeout(function(){
+		  			$scope.set_bgrd_image = function(){
+		  					return {  "background": "center url('/images/iornbars4.jpg')",
+		  						  "background-size": "cover",
+		  						   "background-repeat": "no-repeat",
+		  						   "-webkit-transition": "all linear 3s",
+		  						   "-moz-transition": "all linear 3s",
+		  						   "-o-transition": "all linear 3s",
+		  						   "transition": "all linear 3s"}			
+		  			}
+		  		}, 10000)		
 
-		$timeout(function(){
-				$scope.set_bgrd_image = function(){
-						return {  "background": "center url('/images/iornbars5.jpeg')",
-							  "background-size": "cover",
-							   "background-repeat": "no-repeat",
-							   "-webkit-transition": "all linear 3s",
-							   "-moz-transition": "all linear 3s",
-							   "-o-transition": "all linear 3s",
-							   "transition": "all linear 3s"}			
-				}
-			}, 14000)		
+		  	$timeout(function(){
+		  			$scope.set_bgrd_image = function(){
+		  					return {  "background": "center url('/images/iornbars5.jpeg')",
+		  						  "background-size": "cover",
+		  						   "background-repeat": "no-repeat",
+		  						   "-webkit-transition": "all linear 3s",
+		  						   "-moz-transition": "all linear 3s",
+		  						   "-o-transition": "all linear 3s",
+		  						   "transition": "all linear 3s"}			
+		  			}
+		  		}, 14000)		
 
-		$timeout(function(){
-				$scope.set_bgrd_image = function(){
-						return {  "background": "center url('/images/iornbars.jpg')",
-							  "background-size": "cover",
-							   "background-repeat": "no-repeat",
-							   "-webkit-transition": "all linear 3s",
-							   "-moz-transition": "all linear 3s",
-							   "-o-transition": "all linear 3s",
-							   "transition": "all linear 3s"}			
-				}
-			}, 18000)
-	}, 25000); 
+		  	$timeout(function(){
+		  			$scope.set_bgrd_image = function(){
+		  					return {  "background": "center url('/images/iornbars.jpg')",
+		  						  "background-size": "cover",
+		  						   "background-repeat": "no-repeat",
+		  						   "-webkit-transition": "all linear 3s",
+		  						   "-moz-transition": "all linear 3s",
+		  						   "-o-transition": "all linear 3s",
+		  						   "transition": "all linear 3s"}			
+		  			}
+		  		}, 18000)
+		  }, 25000); 
 
+	});
+	
 	//Render heading text 
 	$scope.texttyping = ["What Are Your Chances?"];
 
